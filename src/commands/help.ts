2e74cdc26d205a2e47a85c8688cdbf6e0673293c
xeltica <xeltica@gmail.com>
2021-03-09 18:26:33 +0900

    help me

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -3,7 +3,21 @@ import { define } from "./define";
 import commands from ".";
 import { getCommandPrefix } from "../misc/env";
 
-export default define('help', '', (_args: string[], _msg: Message, _client: Client) => {
+const helpMeReply = [
+    '自分で何とかしろ',
+    'どうしたん',
+    '大丈夫？爆破する？^^',
+    'とりあえず一発爆ぜとく？ｗ',
+    'shhhhh... 🤯',
+    '爆破は救済ですよ',
+    'リフォームしますか？',
+];
+
+export default define('help', '', (args: string[], _msg: Message, _client: Client) => {
+    const [arg] = args;
+    if (arg === 'me') {
+        return helpMeReply[Math.floor(Math.random() * helpMeReply.length)];
+    }
     const prefix = getCommandPrefix();
     const a = commands.filter(c => !c.hidden).map(c => `${prefix}${c.name}: ${c.description}`).join('\n');
     return a;
