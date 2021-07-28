70c693d7697e9c3b30e23b5fde661654cfd6c0bb
xeltica <xeltica@gmail.com>
2021-07-28 22:26:01 +0900

    開発サーバーサポート

--- src/misc/env.ts
+++ src/misc/env.ts
@@ -7,6 +7,8 @@ export const getTosMcserverChannel = () => process.env.TOS_MCSERVER_CHANNEL;
 export const getRoleChannel = () => process.env.ROLE_CHANNEL;
 export const getClubChannel = () => process.env.CLUB_CHANNEL;
 export const getSkinChannel = () => process.env.SKIN_CHANNEL;
+export const getGameChatChannel = () => process.env.GAME_CHAT_CHANNEL;
+export const getConsoleChannel = () => process.env.CONSOLE_CHANNEL;
 export const getTodoChannel = () => process.env.TODO_CHANNEL;
 export const getCommandPrefix = () => process.env.COMMAND_PREFIX ?? '!';
 export const getAdmins = () => (process.env.ADMINS ?? '').split(',');
--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -7,10 +7,12 @@ import command from "./command";
 import uploadSkin from "./upload-skin";
 import react from "./react";
 import todo from "./todo";
+import observeDiskFull from "./observe-disk-full";
 
 export const plugins: Plugin[] = [
     command,
     react,
     uploadSkin,
     todo,
+    observeDiskFull,
 ];
