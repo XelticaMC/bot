3ea34d91de1affc5663ca19607625a2c8308134c
xeltica <xeltica@gmail.com>
2021-08-03 21:26:01 +0900

    ロール機能を廃止

--- src/plugins/react.ts
+++ src/plugins/react.ts
@@ -1,13 +1,11 @@
 /* リアクションプラグイン
  * リアクションを処理するプラグインです
- * TODO: 規約プラグインとロールプラグインに分ける
  */
 
 import { Client, MessageReaction, PartialUser, User } from "discord.js";
-import { clubs } from "../misc/clubs";
-import { getClubChannel, getRoleChannel, getTosChannel, getTosMcserverChannel } from "../misc/env";
+import { getTosChannel, getTosMcserverChannel } from "../misc/env";
 import { getRole } from "../misc/getRole";
-import { citizenRole, memberRole, roles } from "../misc/roles";
+import { citizenRole, memberRole } from "../misc/roles";
 import { define } from "./Plugin";
 
 export default define({
@@ -46,96 +44,6 @@ export default define({
                     .then((m) => console.info(`Made ${m.displayName} as a citizen.`))
                     .catch((e) => console.error(`Failed to make ${member.displayName} as a citizen because of ${e.name ?? 'Error'}: ${e.message ?? 'unknown'}.`));
             }
-        }
-
-        if (reaction.message.channel.id === getRoleChannel() && user.id !== cli.user?.id) {
-             // サーバーでなければ無視
-            if (!reaction.message.guild) return;
-
-            // リアクションに対応するロール定義を取得。ロールに対応しないリアクションは無視
-            const r = roles.find(r => r.emoji === reaction.emoji.toString());
-            if (!r) return;
-
-            // 定義に対応するサーバー内のロールを取得。無い場合は警告する
-            const role = await getRole(reaction.message.guild, r);
-            if (!role) {
-                console.warn(`Role ${r.name} doesn't exist on your server!`);
-                return;
-            };
-
-            // リアクションの送り主にロールを付与する
-            const member = await reaction.message.guild?.members.fetch(user as User);
-            member?.roles.add(role)
-                .then((m) => console.info(`Gave ${m.displayName} a role "${r.name}".`))
-                .catch((e) => console.error(`Failed to give ${member.displayName} as role "${r.name}" because of ${e.name ?? 'Error'}: ${e.message ?? 'unknown'}.`));
-        }
-
-        if (reaction.message.channel.id === getClubChannel() && user.id !== cli.user?.id) {
-            // サーバーでなければ無視
-            if (!reaction.message.guild) return;
-
-            // リアクションに対応するロール定義を取得。ロールに対応しないリアクションは無視
-            const r = clubs.find(r => r.emoji === reaction.emoji.toString());
-            if (!r) return;
-
-            // 定義に対応するサーバー内のロールを取得。無い場合は警告する
-            const role = await getRole(reaction.message.guild, r);
-            if (!role) {
-                console.warn(`Role ${r.name} doesn't exist on your server!`);
-                return;
-            };
-
-            // リアクションの送り主にロールを付与する
-            const member = await reaction.message.guild?.members.fetch(user as User);
-            member?.roles.add(role)
-                .then((m) => console.info(`Gave ${m.displayName} a role "${r.name}".`))
-                .catch((e) => console.error(`Failed to give ${member.displayName} as role "${r.name}" because of ${e.name ?? 'Error'}: ${e.message ?? 'unknown'}.`));
-        }
-    },
-
-    async onReactionRemoved(reaction: MessageReaction, user: User | PartialUser, cli: Client) {
-        // ロール剥奪
-        if (reaction.message.channel.id === getRoleChannel() && user.id !== cli.user?.id) {
-            // サーバーでなければ無視
-            if (!reaction.message.guild) return;
-
-            // リアクションに対応するロール定義を取得。ロールに対応しないリアクションは無視
-            const r = roles.find(r => r.emoji === reaction.emoji.toString());
-            if (!r) return;
-
-            // 定義に対応するサーバー内のロールを取得。無い場合は警告する
-            const role = await getRole(reaction.message.guild, r);
-            if (!role) {
-                console.warn(`Role ${r.name} doesn't exist on your server!`);
-                return;
-            };
-
-            // リアクションの送り主からロールを剥奪する
-            const member = await reaction.message.guild?.members.fetch(user as User);
-            member?.roles.remove(role)
-                .then((m) => console.info(`Took ${m.displayName} a role "${r.name}".`))
-                .catch((e) => console.error(`Failed to take ${member.displayName} a role "${r.name}" because of ${e.name ?? 'Error'}: ${e.message ?? 'unknown'}.`));
-        }
-        if (reaction.message.channel.id === getClubChannel() && user.id !== cli.user?.id) {
-            // サーバーでなければ無視
-            if (!reaction.message.guild) return;
-
-            // リアクションに対応するロール定義を取得。ロールに対応しないリアクションは無視
-            const r = clubs.find(r => r.emoji === reaction.emoji.toString());
-            if (!r) return;
-
-            // 定義に対応するサーバー内のロールを取得。無い場合は警告する
-            const role = await getRole(reaction.message.guild, r);
-            if (!role) {
-                console.warn(`Role ${r.name} doesn't exist on your server!`);
-                return;
-            };
-
-            // リアクションの送り主からロールを剥奪する
-            const member = await reaction.message.guild?.members.fetch(user as User);
-            member?.roles.remove(role)
-                .then((m) => console.info(`Took ${m.displayName} a role "${r.name}".`))
-                .catch((e) => console.error(`Failed to take ${member.displayName} a role "${r.name}" because of ${e.name ?? 'Error'}: ${e.message ?? 'unknown'}.`));
         }
     },
 });
