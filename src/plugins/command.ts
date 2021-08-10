810bde979cd7a75aadbd3985753e539e3ecb3ab2
xeltica <xeltica@gmail.com>
2021-08-10 09:24:23 +0900

    サーバー内からコマンドを呼べるように

--- src/plugins/command.ts
+++ src/plugins/command.ts
@@ -7,10 +7,17 @@ import { Client, Message } from "discord.js";
 import { getCommand } from "../commands";
 import { define } from "./Plugin";
 
+const prefix = process.env.COMMAND_PREFIX ?? '!!';
+
+const sanitizeEndermanText = (text: string): string => {
+    if (text.startsWith(prefix)) return text;
+    const i = text.indexOf('»');
+    return i < 0 ? text : text.slice(i + 1).trim();
+};
+
 export default define({
     async onMessage(msg: Message, cli: Client): Promise<void> {
-        const prefix = process.env.COMMAND_PREFIX ?? '!!';
-        const text = msg.content;
+        const text = sanitizeEndermanText(msg.content);
         const ch = msg.channel;
         if (text.startsWith(prefix)) {
             const args = text.substring(prefix.length).split(' ');
